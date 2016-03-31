2e2860d6354e31dd6fb23ed598a35357fa5d4bd0
fayaz <faizvf@gmail.com>
2016-03-30 23:03:55 -0400

    working harris corner

--- convolution.m
+++ convolution.m
@@ -1,4 +1,5 @@
 function [result] = convolution(img0, h)
+    
     result = double(zeros(size(img0)));
     
     M = floor(size(h, 1)/2);
@@ -9,6 +10,7 @@ function [result] = convolution(img0, h)
     img = horzcat(zeros(imRows, M), img0, zeros(imRows, M));
     img = vertcat(zeros(N, imCols+2*M), img, zeros(N, imCols+2*M));
     
+    h = rot90(h, 2);
     for i = 1:size(img, 1)-2*N
         for j = 1:size(img, 2)-2*M
             result(i,j) = sum(sum(h .* double(img(i:i+2*N, j:j+2*M))));
---
+++ myHarrisCorner.m
@@ -0,0 +1,35 @@
+function [t1, t2, R] = myHarrisCorner(Ix, Iy, k, image)
+    
+    img = double(imread(image));
+    
+    [~, ~, Ix, Iy] = myEdgeFilter(img, 1);
+    Ix = mat2gray(abs(Ix));
+    Iy = mat2gray(abs(Iy));
+        
+    Ix2 = Ix .^ 2;
+    Iy2 = Iy .^ 2;
+    Ixy = Ix .* Iy;
+    
+    Ix2 = convolution(Ix2, gaussianKernel(1));
+    Iy2 = convolution(Iy2, gaussianKernel(1));
+    Ixy = convolution(Ixy, gaussianKernel(1));
+        
+    t1 = (Ix2.*Iy2 - Ixy.^2); 
+    t2 = (Ix2 + Iy2).^2;
+    %R = im2bw(mat2gray(abs(t1 - k .* t2)), 0.4);
+    R = im2bw(mat2gray(abs(t1 - k .* t2)), 0.6);
+    
+    figure;
+    imshow(imread(image));
+    hold on
+    [row, col] = find(R == 1);
+    plot(col, row, 'r+');
+    
+end
+
+
+function [gau] = gaussianKernel(sigma)
+     halfwid = floor(3*sigma);
+     [xx,yy] = meshgrid(-halfwid:halfwid, -halfwid:halfwid);
+     gau = exp(-1/(2*sigma^2) * (xx.^2 + yy.^2));
+end
